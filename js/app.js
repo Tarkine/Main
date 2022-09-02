a41e30bfa900125394542096b894f21fbc5f7621
Apokoala <michael_gazaway@hotmail.com>
2022-09-01 21:55:05 -0400

    storyarray

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
 //object literals to organize the game pages
 
 const page1 = {
-    text = '',
+    text = storyContentArray[0],
     choice1 = dialogue[0],
     choice2 = dialogue[2],
     response1 = dialogue[1],
@@ -16,7 +16,7 @@ const page1 = {
 }
 
 const page2 = {
-    text = '',
+    text = storyContentArray[1],
     choice1 = dialogue[3],
     choice2 = dialogue[4],
     response1 = '',
@@ -27,7 +27,7 @@ const page2 = {
 }
 
 const page3 = {
-    text = '',
+    text = storyContentArray[2],
     choice1 = dialogue[3],
     choice2 = dialogue[4],
     response1 = '',
@@ -60,7 +60,7 @@ const page5 = {
 }
 
 const page6 = {
-    text = '',
+    text = storyContentArray[3],
     choice1 = dialogue[5],
     choice2 = dialogue[6],
     response1 = dialogue[7],
@@ -71,7 +71,7 @@ const page6 = {
 }
 
 const page7 = {
-    text = '',
+    text = storyContentArray[4],
     choice1 = dialogue[8],
     choice2 = dialogue[9],
     response1 = '',
@@ -93,7 +93,7 @@ const page8 = {
 }
 
 const page9 = {
-    text = '',
+    text = storyContentArray[5],
     choice1 = dialogue[11],
     choice2 = '',
     response1 = '',
@@ -137,7 +137,7 @@ const page12 = {
 }
 
 const page13 = {
-    text = '',
+    text = storyContentArray[6],
     choice1 = dialogue[14],
     choice2 = dialogue[15],
     response1 = '',
@@ -148,7 +148,7 @@ const page13 = {
 }
 
 const page14 = {
-    text = '',
+    text = storyContentArray[7],
     choice1 = dialogue[14],
     choice2 = '',
     response1 = '',
@@ -170,7 +170,7 @@ const page15 = {
 }
 
 const page16 = {
-    text = '',
+    text = storyContentArray[8],
     choice1 = dialogue[16],
     choice2 = dialogue[18],
     response1 = dialogue[17],
@@ -203,7 +203,7 @@ const page18 = {
 }
 
 const page19 = {
-    text = '',
+    text = storyContentArray[9],
     choice1 = '',
     choice2 = '',
     response1 = dialogue[20],
@@ -214,7 +214,7 @@ const page19 = {
 }
 
 const page20 = {
-    text = '',
+    text = storyContentArray[10],
     choice1 = dialogue[22],
     choice2 = dialogue[23],
     response1 = '',
@@ -258,7 +258,7 @@ const page23 = {
 }
 
 const page24 = {
-    text = '',
+    text = storyContentArray[11],
     choice1 = dialogue[26],
     choice2 = dialogue[27],
     response1 = '',
@@ -269,7 +269,7 @@ const page24 = {
 }
 
 const page25 = {
-    text = '',
+    text = storyContentArray[12],
     choice1 = dialogue[14],
     choice2 = dialogue[29],
     response1 = '',
@@ -280,7 +280,7 @@ const page25 = {
 }
 
 const page26 = {
-    text = '',
+    text = storyContentArray[13],
     choice1 = dialogue[30],
     choice2 = dialogue[31],
     response1 = '',
@@ -291,7 +291,7 @@ const page26 = {
 }
 
 const page27 = {
-    text = '',
+    text = storyContentArray[14],
     choice1 = dialogue[32],
     choice2 = dialogue[33],
     response1 = '',
@@ -302,7 +302,7 @@ const page27 = {
 }
 
 const page28 = {
-    text = '',
+    text = storyContentArray[15],
     choice1 = dialogue[34],
     choice2 = dialogue[14],
     response1 = '',
@@ -313,7 +313,7 @@ const page28 = {
 }
 
 const page29 = {
-    text = '',
+    text = storyContentArray[16],
     choice1 = dialogue[35],
     choice2 = dialogue[36],
     response1 = '',
@@ -324,7 +324,7 @@ const page29 = {
 }
 
 const page30 = {
-    text = '',
+    text = storyContentArray[17],
     choice1 = dialogue[37],
     choice2 = dialogue[38],
     response1 = '',
@@ -335,7 +335,7 @@ const page30 = {
 }
 
 const page31 = {
-    text = '',
+    text = storyContentArray[18],
     choice1 = dialogue[39],
     choice2 = dialogue[40],
     response1 = '',
@@ -346,7 +346,7 @@ const page31 = {
 }
 
 const page32 = {
-    text = '',
+    text = storyContentArray[19],
     choice1 = dialogue[39],
     choice2 = dialogue[40],
     response1 = '',
@@ -357,7 +357,7 @@ const page32 = {
 }
 
 const page33 = {
-    text = '',
+    text = storyContentArray[20],
     choice1 = dialogue[41],
     choice2 = '',
     response1 = '',
@@ -368,7 +368,7 @@ const page33 = {
 }
 
 const page34 = {
-    text = '',
+    text = storyContentArray[21],
     choice1 = dialogue[42],
     choice2 = dialogue[43],
     response1 = '',
@@ -379,7 +379,7 @@ const page34 = {
 }
 
 const page35 = {
-    text = '',
+    text = storyContentArray[22],
     choice1 = '',
     choice2 = '',
     response1 = '',
@@ -388,6 +388,43 @@ const page35 = {
     userinterface = '',
     element1 = ''
 }
+
+const page35 = {
+    text = storyContentArray[23],
+    choice1 = '',
+    choice2 = '',
+    response1 = '',
+    response2 = '',
+    background = '',
+    userinterface = '',
+    element1 = ''
+}
+
+let storyContentArray = [
+    'You\'re in a small plane and you crash...Sad...You wake up and look around for any other surviving passengers. You see one surviving passenger, unconcious but still breathing. Their arm looks broken and is bleeding profusely. Gross. Do you spend time looking for resources to create a splint and try to nurse them back to health?',
+    'You look around and see a dense tropical forest. There is a trail in front of you. Outstanding! A trail means people!',
+    'You look around and see a dense tropical forest. There is a trail in front of you. Outstanding! A trail means people! You remember that you have so much to live for and the new season of survivor drops in October.',
+    'You come across some ancient ruins. There are vines growing up the broken walls. It does not appear as if anyone has been here in quite some time. You begin to worry that these paths may not actually lead to civilization.',
+    'You touch the symbol and it starts to glow. You start to feel like you should subscribe to youtube red...you recognize the idea as stupid and quickly shake it from your mind. You notice the stone is loose.',
+    'After wandering along the path for what seemed like tens of minutes. You reach a large wall. The wall stretches into the dark forest. On the path is a large gate with no handles. You notice on the left side of the gate a recess that is the exact size of the stone you have in your pocket.',
+    'You come across a river. A BOAT!!! You race over to it and start to take inventory of it. It\'s missing its keys,and the steering wheel is gone. Also you notice both fuses have been ripped out. You look at the river, wondering if you could use a log to float down the river. At that moment a sloth from an overhanging vine drops into the water. A flurry of froth and blood erupt as bloodthirsty pirannahs rip it to shreds. Sad...gross...Looks like fixing the boat is the only option. You notice a fuse sitting next to the boat...someone must have removed them. You pick up the fuse and put it in your pocket.',
+    'You place the fuse in the boat. Amazing. You congratulate yourself on your own ingenuity. Perhaps there are more pieces of the boat somewhere in the forest.',
+    'You come to a fork in the road! The right path looks much like the trial you\'ve already seen. The left path is dark. Vines hang down to the trail from the canopy above. You hear the sound of a leopard screaming.',
+    'You reach a clearing in the forest and see a pile of debris washed up on the bank of a river. You\'ll need to dig through the pile to see if there is anything of interest.',
+    'You  found a fuse that matched the one you placed in the boat earlier.', 'You exit the forest and find youself in a clearing. You find, after a quick survey of the clearing, an urn. It has a small but sealed opening flanked on either side by recesses the same size as the one that was in the gate.',
+    'You stand up...genious...The other stone must be around here somewhere. You think a moment and realize you know where another stone is. You left it in on the other side of the gate.',
+    'You find yourself back at the gate. But how to get over it? You grab a branch and start digging furiously...many hours later you emerge on the other side of the wall. Man...walls are stupid...and anyone who thinks they can keep someone out does not have a good grasp of verticle spaces.',
+    'You find yourself back at the gate.',
+    'You find yourself back in the second clearing. You place the stone into the second recess and the urn....clicks...I guess...Ethan...You look inside and you find the steering wheel for the boat. You wonder how it got here. But you remember they didnt explain anything on “Lost” either...oh well... You cant fit the steering wheel in your cargo pants so you shove it down the front of them.',
+    'While on the trail you hear the cawing of a strange bird. Clearly of the covid family...and unknown to you...this bird species was quite rare. Having been the first birds to lay their eggs mid air in order to control their own population. In its beak you spot shiny and keylike objects...you wonder what they are. Regardless, you must have them.',
+    'You miss...the bird starts to fly away, laying and losing its next generation in its starled state. Memories of being cut from your junior varsity baseball team rush back.',
+    'You catch up to the bird.',
+    'You miss. The bird flies away.',
+    'You hit the bird...and amazingly...hit a second bird, which laid and lost its next generation in its starled state. You run up to the two birds you\’ve stunned with one stone...it\’s just like the old idiom: “You can lead a horse to water but you shouldn\'t use it like a straw.” If only Adam was recording this.',
+    'You enter the clearing with the boat.',
+    'You start your repaired boat, first try...amazing that it had gas, too. You find civilization and are arrested for trespassing and grand larceny. Technically...you are now a Pirate...and that is pretty cool.',
+    'In loving memory of Ben Arno...hes not dead...we just like remembering him. Big shoutout to Google and the stack overflow crew, hawaiian shirts, and a very lenient late work policy.'
+];
 
 
 //Create array for dialogue and stored it locally, validated.
@@ -501,7 +538,7 @@ console.log(items[0])
 //inventory button that displays the inventory on click and makes it disappear when clicked again.
 //alright how the fuck do I do this...
 
-let inventory = (JSON.parse(localStorage.getItem('inventory')) ? JSON.parse(localStorage.getItem('inventory')) : []); //ternary "?" if the condition (first) is true then action (second) position ":" same as else (set inventory)so that on refresh it wont reset array
+let inventory = (JSON.parse(localStorage.getItem('inventory')) ? JSON.parse(localStorage.getItem('inventory')) : []); //ternary "?" if the condition (first) is true then action (second) position ":" same as else (set inventory)so that on refresh it wont reset array. I set the variable this way to account for refresh...rather than overwriting on refresh it will set it to local storage. 
 function setItemInventory() {
     localStorage.setItem('inventory', JSON.stringify(inventory));
 }
